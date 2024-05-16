cd2e7e4d9a18631b4229bc04f88a61f0c1d9a7b2
Katharina Weber <s52785@edu.campus02.at>
2024-05-16 23:18:27 +0200

    added Log4j to the configuration and added a Logging Object in the main Method

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,15 +1,23 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
+        logger.error("Start of the main method");
         double num1=1.5;
         double num2=2.5;
 
         System.out.println(Calculator.add(num1 ,num2));
         System.out.println(Calculator.minus(num1,num2));
+
         System.out.println(Calculator.multiply(num1 ,num2));
         System.out.println(Calculator.divide(num1 ,num2));
 
         System.out.println("Katharina Weber");
+        logger.info("End of the main method");
     }
 }
